6debe273b85f77bfee5f5309ce6a8d4315938f1e
Amb1valence <mikkiwOw@bk.ru>
2016-06-07 23:08:39 +0300

    First Eclipse Commit

---
+++ gdik8lc/gdik8lc.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : gdik8lc.cpp
+// Author      : Mikhail Kiselyov, Alexander Moroz.
+// Version     : 1.0.
+// Copyright   : 07.06.2016.
+// Description : Ansi-style.
+//============================================================================
+
+//For First Commit.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	return 0;
+}
